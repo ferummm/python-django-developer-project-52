1d7dd6c592996adcadbb7cf64a0afca3def40e74
Andreeva Polina <ferummm.it@gmail.com>
2023-09-02 15:18:15 +0300

    fix settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'webserver']
 CSRF_TRUSTED_ORIGINS = ['https://task-manager-django-production.up.railway.app']
